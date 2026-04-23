3c5a09c876e1b6a02d76ab11422341f4bff2adfd
Sean <mckinneysean91@gmail.com>
2026-04-23 19:48:43 +0200

    Added test func in app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 from flask import Flask, render_template
 import plotly.express as px 
 from modules.natural_disaster import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
